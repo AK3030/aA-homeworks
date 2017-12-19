986ea635c51f321118911075e96de2b744ed55e7
Alex Kite <alexkite511@gmail.com>
2017-12-19 09:51:56 -0800

    add Playwright class

--- W3D2/ORM_practice_hw/plays.rb
+++ W3D2/ORM_practice_hw/plays.rb
@@ -72,3 +72,63 @@ class Play
     SQL
   end
 end
+
+class Playwright
+  def self.all
+    data = PlayDBConnection.intance.execute("SELECT * FROM playwrights")
+    data.map {|datum| Playwright.new(datum)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def self.find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+  end
+
+  def self.get_plays(playwright_name)
+    PlayDBConnection.instance.execute(<<-SQL, playwright_name)
+      SELECT
+        title
+      FROM
+        plays
+      JOIN playwrights
+        ON plays.playwright_id = playwrights.id
+      WHERE
+        name = ?
+    SQL
+  end
+
+  def create
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (id, name, birth_year)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+
+end
